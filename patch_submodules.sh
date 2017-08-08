4341773e134f16f5de6432705c0801c331209874
Julio B <julio.bacel@gmail.com>
2017-08-08 19:55:22 +0300

    Fix patch_submodules script

--- patch_submodules.sh
+++ patch_submodules.sh
@@ -4,12 +4,18 @@ BASE16SHELL_DIR=$DOTSDIR/base16-shell/.config/base16-shell/
 
 # Update submodules to latest commit
 git -C "$DOTSDIR" submodule update --init --recursive --remote
+if [ $? -ne 0 ];
+then
+	echo "Error: Submodule update failed" >>/dev/stderr
+	exit 2
+fi
+
 git -C "$DOTSDIR" add "$DOTSDIR/base16-shell/.config/base16-shell"
 
 # Patch base16shell
 if [ ! -f $DOTSDIR/0001-Update-i3-xresources-themes-with-profile_helper.patch ];
 then
-	echo "No file patch file!" >>/dev/stderr
+	echo "Error: No patch file!" >>/dev/stderr
 	exit 1
 fi
 git -C "$BASE16SHELL_DIR" am -3 < $DOTSDIR/0001-Update-i3-xresources-themes-with-profile_helper.patch
