db7b8d90f71ce7fa18b6a359a2142f96532a91c2
Julio B <julio.bacel@gmail.com>
2017-08-08 20:42:42 +0300

    Update redshift

--- .config/redshift/hooks/notify.sh
+++ .config/redshift/hooks/notify.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 case $1 in
 	period-changed)
-		exec notify-send --app-name="redshift" --expire-time=3000 --urgency=low "Redshift" "Period changed to $3"
+		exec notify-send --app-name="redshift" --icon="/usr/share/icons/hicolor/scalable/apps/redshift.svg" --expire-time=3000 --urgency=low "Redshift" "Period changed to $3"
 esac
