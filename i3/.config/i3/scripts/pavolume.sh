07006ffa9f45ea96e3eae57c6be96b0feabc95c0
Julio B <julio.bacel@gmail.com>
2017-09-11 23:04:34 +0300

    polybar: update bar modules/colors and pavolume script

--- i3/.config/i3/scripts/pavolume.sh
+++ i3/.config/i3/scripts/pavolume.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #####https://github.com/jaagr/polybar/wiki/User-contributed-modules#pulseaudio-volume
 # finds the active sink for pulse audio and increments the volume. useful when you have multiple audio outputs and have a key bound to vol-up and down
 
@@ -6,39 +6,46 @@ osd='no'
 inc='2'
 capvol='no'
 maxvol='200'
-tmpfile='/tmp/pasink.tmp'
 autosync='no'
 
-active_sink=`pacmd list-sinks |awk '/* index:/{print $3}'`
-limit=$(expr 100 - ${inc})
-maxlimit=$(expr ${maxvol} - ${inc})
+# Muted status
+# yes: muted
+# no : not muted
+curStatus="no"
+active_sink=""
+limit=$((100 - inc))
+maxlimit=$((maxvol - inc))
+
+reloadSink() {
+	active_sink=$(pacmd list-sinks | awk '/* index:/{print $3}')
+}
 
 function volUp {
 
 	getCurVol
 
-	if [ ${capvol} = 'yes' ]
+	if [ "$capvol" = 'yes' ]
 	then
-		if [ ${curVol} -le 100 -a ${curVol} -ge ${limit} ]
+		if [ "$curVol" -le 100 ] && [ "$curVol" -ge "$limit" ]
 		then
-			pactl set-sink-volume ${active_sink} -- 100%
-		elif [ ${curVol} -lt ${limit} ]
+			pactl set-sink-volume "$active_sink" -- 100%
+		elif [ "$curVol" -lt "$limit" ]
 		then
-			pactl set-sink-volume ${active_sink} -- +${inc}%
+			pactl set-sink-volume "$active_sink" -- "+$inc%"
 		fi
-	elif [ ${curVol} -le ${maxvol} -a ${curVol} -ge ${maxlimit} ]
+	elif [ "$curVol" -le "$maxvol" ] && [ "$curVol" -ge "$maxlimit" ]
 	then
-		pactl set-sink-volume ${active_sink} ${maxvol}%
-	elif [ ${curVol} -lt ${maxlimit} ]
+		pactl set-sink-volume "$active_sink" "$maxvol%"
+	elif [ "$curVol" -lt "$maxlimit" ]
 	then
-		pactl set-sink-volume ${active_sink} +${inc}%
+		pactl set-sink-volume "$active_sink" "+$inc%"
 	fi
 
 	getCurVol
 
 	if [ ${osd} = 'yes' ]
 	then
-		qdbus org.kde.kded /modules/kosd showVolume ${curVol} 0
+		qdbus org.kde.kded /modules/kosd showVolume "$curVol" 0
 	fi
 
 	if [ ${autosync} = 'yes' ]
@@ -49,12 +56,12 @@ function volUp {
 
 function volDown {
 
-	pactl set-sink-volume ${active_sink} -${inc}%
+	pactl set-sink-volume "$active_sink" "-$inc%"
 	getCurVol
 
 	if [ ${osd} = 'yes' ]
 	then
-		qdbus org.kde.kded /modules/kosd showVolume ${curVol} 0
+		qdbus org.kde.kded /modules/kosd showVolume "$curVol" 0
 	fi
 
 	if [ ${autosync} = 'yes' ]
@@ -65,39 +72,32 @@ function volDown {
 }
 
 function getSinkInputs {
-
-	inputs=`pacmd list-sink-inputs |grep -B 4 'sink: '${1}' ' |awk '/index:/{print $2}' >${tmpfile}`
-	input_array=`cat $tmpfile`
+	input_array=$(pacmd list-sink-inputs | grep -B 4 "sink: $1 " | awk '/index:/{print $2}')
 }
 
 function volSync {
-
-	getSinkInputs ${active_sink}
+	getSinkInputs "$active_sink"
 	getCurVol
 
-	for each in ${input_array}
+	for each in $input_array
 	do
-		pactl set-sink-input-volume ${each} ${curVol}%
+		pactl set-sink-input-volume "$each" "$curVol%"
 	done
-
 }
 
 function getCurVol {
-
-	curVol=`pacmd list-sinks |grep -A 15 'index: '${active_sink}'' |grep 'volume:' |egrep -v 'base volume:' |awk -F : '{print $3}' |grep -o -P '.{0,3}%'|sed s/.$// |tr -d ' '`
-
+	curVol=$(pacmd list-sinks | grep -A 15 'index: '"$active_sink"'' | grep 'volume:' | grep -E -v 'base volume:' | awk -F : '{print $3}' | grep -o -P '.{0,3}%'| sed s/.$// | tr -d ' ')
 }
 
 function volMute {
-
 	case "$1" in
 		mute)
-			pactl set-sink-mute ${active_sink} 1
+			pactl set-sink-mute "$active_sink" 1
 			curVol=0
 			status=1
 			;;
 		unmute)
-			pactl set-sink-mute ${active_sink} 0
+			pactl set-sink-mute "$active_sink" 0
 			getCurVol
 			status=0
 			;;
@@ -111,11 +111,60 @@ function volMute {
 }
 
 function volMuteStatus {
-
-	curStatus=`pacmd list-sinks |grep -A 15 'index: '${active_sink}'' |awk '/muted/{ print $2}'`
-
+	curStatus=$(pacmd list-sinks | grep -A 15 "index: $active_sink" | awk '/muted/{ print $2}')
 }
 
+# Prints output for bar
+# Listens for events for fast update speed
+function listen {
+	firstrun=0
+
+	pactl subscribe 2>/dev/null | {
+	while true; do
+		{
+			# If this is the first time just continue
+			# and print the current state
+			# Otherwise wait for events
+			# This is to prevent the module being empty until
+			# an event occurs
+			if [ $firstrun -eq 0 ]
+			then
+				firstrun=1
+			else
+				read -r event || break
+				if ! echo "$event" | grep -e "on card" -e "on sink"
+				then
+					# Avoid double events
+					continue
+				fi
+			fi
+		} &>/dev/null
+		output
+	done
+}
+}
+
+function output() {
+	reloadSink
+	getCurVol
+	volMuteStatus
+	if [ ${curStatus} = 'yes' ]
+	then
+		echo "%{F#f00} $curVol%{F-}"
+	else
+		if [ ${curVol} -le 30 ]
+		then
+			echo " $curVol"
+		elif [ ${curVol} -le 60 ]
+		then
+			echo " $curVol"
+		else
+			echo " $curVol"
+		fi
+	fi
+} #}}}
+
+reloadSink
 case "$1" in
 	--up)
 		volUp
@@ -125,7 +174,7 @@ case "$1" in
 		;;
 	--togmute)
 		volMuteStatus
-		if [ ${curStatus} = 'yes' ]
+		if [ "$curStatus" = 'yes' ]
 		then
 			volMute unmute
 		else
@@ -141,22 +190,13 @@ case "$1" in
 	--sync)
 		volSync
 		;;
+	--listen)
+		# Listen for changes and immediately create new output for the bar
+		# This is faster than having the script on an interval
+		listen
+		;;
 	*)
-		getCurVol
-		volMuteStatus
-		if [ ${curStatus} = 'yes' ]
-		then
-			echo "%{F#f00} $curVol%{F-}"
-		else
-			if [ ${curVol} -le 30 ]
-			then
-				echo " $curVol"
-			elif [ ${curVol} -le 60 ]
-			then
-				echo " $curVol"
-			else
-				echo " $curVol"
-			fi
-		fi
+		# By default print output for bar
+		output
 		;;
 esac
