7e5cff796423de0cb019075650f2dd9b299f87ae
Julio B <julio.bacel@gmail.com>
2018-09-09 20:14:43 +0300

    base16: update hooks

--- base16-shell/.config/base16-shell-hooks/i3wm_colors.sh
+++ base16-shell/.config/base16-shell-hooks/i3wm_colors.sh
@@ -4,6 +4,6 @@ if [ -x $HOME/.config/i3/scripts/update_color_theme.sh ]; then
 	if [ $? -eq 0 ]; then
 		(echo "`date`: Base16 theme reload " && i3-msg reload) >> $HOME/.config/i3/stdout
 	else
-		notify-send "Theme base16-$BASE16_THEME does not exist for i3wm"
+		notify-send --urgency=critical --icon="/usr/share/icons/Arc/emblems/64/emblem-important.png" --expire-time=3500 "Theme base16-$BASE16_THEME does not exist for i3wm"
 	fi
 fi
--- base16-shell/.config/base16-shell-hooks/xresources.sh
+++ base16-shell/.config/base16-shell-hooks/xresources.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #Update xresources
-TMPFILE=`mktemp -q /tmp/Xresources.XXXXXXXXXX` \
- && ( [ -f $HOME/.config/base16-xresources/xresources/base16-$BASE16_THEME-256.Xresources ] \
- && (head -n -2 $HOME/.Xresources && echo -e "#include \".config/base16-xresources/xresources/base16-$BASE16_THEME-256.Xresources\"\n! vim:ft=xdefaults" ) > $TMPFILE \
- && cp $TMPFILE $HOME/.Xresources && xrdb $HOME/.Xresources ) \
-  || notify-send "Theme base16-$BASE16_THEME does not exist for Xresources"
+( [ -f $HOME/.config/base16-xresources/xresources/base16-$BASE16_THEME-256.Xresources ] \
+ && ln -sf xresources/base16-$BASE16_THEME-256.Xresources $HOME/.config/base16-xresources/base16.Xresources \
+ && xrdb $HOME/.Xresources \
+) || notify-send --urgency=critical --icon="/usr/share/icons/Arc/emblems/64/emblem-important.png" --expire-time=3500 "Theme base16-$BASE16_THEME does not exist for Xresources"
