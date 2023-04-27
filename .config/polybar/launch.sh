b76cc29c7beb2b127881330085b19c2ef671c5f2
Julio B <julio.bacel@gmail.com>
2023-04-27 03:55:31 +0300

    polybar: simplify config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,5 +2,8 @@
 
 # Terminate already running bar instances
 killall -q polybar
+#[ -f "/run/user/$(id -u)/polybar/traybar-pid" ] && rm "/run/user/$(id -u)/polybar/traybar-pid"
 
-polybar top
+polybar top & disown
+#polybar tray & disown && echo $! > "/run/user/$(id -u)/polybar/traybar-pid"
+polybar top2 & disown
