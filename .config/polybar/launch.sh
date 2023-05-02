f394d377c651bb5912d9e175622213e1b4937cdf
Julio B <julio.bacel@gmail.com>
2023-05-02 23:07:09 +0300

    polybar: minor fixes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,8 +2,6 @@
 
 # Terminate already running bar instances
 killall -q polybar
-#[ -f "/run/user/$(id -u)/polybar/traybar-pid" ] && rm "/run/user/$(id -u)/polybar/traybar-pid"
 
-polybar top & disown
-#polybar tray & disown && echo $! > "/run/user/$(id -u)/polybar/traybar-pid"
-polybar top2 & disown
+polybar --reload top & disown
+polybar --reload top2 & disown
