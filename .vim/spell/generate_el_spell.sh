14226b2d710f1aca9fab15b3d6e8a99de40420b7
Julio B <julio.bacel@gmail.com>
2023-05-21 05:15:18 +0300

    Remove vim

--- .vim/spell/generate_el_spell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -Lo orthos-el_GR-latest.oxt https://sourceforge.net/projects/orthos-spell/files/latest/download \
-&& 7z x orthos-el_GR-latest.oxt dicts/el_GR.* \
-&& nvim -c 'mkspell! el dicts/el_GR | q' \
-&& rm orthos-el_GR-latest.oxt && rm -r dicts
