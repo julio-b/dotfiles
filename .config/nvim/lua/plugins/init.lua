f3bd65c9eac22e58bb55e99c43686a4c850db9a6
Julio B <julio.bacel@gmail.com>
2023-06-13 05:17:39 +0300

    nvim: source theme options during startup

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,7 +4,7 @@ return {
 		"mcchrish/zenbones.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
+		init = function()
 			-- load the colorscheme here
 			vim.g.bones_compat = 1
 			vim.o.background = 'light'
