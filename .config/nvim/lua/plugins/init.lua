2d5a08e4a010e2383c955436dd7ad8545cb2078d
Julio B <julio.bacel@gmail.com>
2023-09-11 03:38:49 +0300

    nvim: fix initialization of vimtex

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -23,8 +23,7 @@ return {
 	{
 		"lervag/vimtex",
 		lazy = false,
-		ft = { "tex" },
-		config = function()
+		init = function()
 			vim.g.vimtex_view_method = "zathura"
 			vim.g.vimtex_compiler_method = "latexmk"
 		end,
